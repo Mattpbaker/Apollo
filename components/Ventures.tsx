e556b72e5a8fc4b54608fdf9c872de024b72038b
Matthew Baker <mattpbaker@icloud.com>
2026-01-16 23:40:02 +0000

    Remove Twitter link for Cleanest Cleaners

--- components/Ventures.tsx
+++ components/Ventures.tsx
@@ -36,7 +36,6 @@ const ventures = [
     description: 'Providing top-quality cleaning services with a focus on excellence and customer satisfaction.',
     image: '/ventures/cleanest-cleaners.jpg',
     socialLinks: {
-      twitter: 'https://twitter.com/cleanestcleaners',
       linkedin: 'https://linkedin.com/company/cleanest-cleaners',
       website: 'https://cleanestcleaners.com',
     },
